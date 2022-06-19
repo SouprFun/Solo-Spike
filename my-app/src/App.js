16166c29661ec6cb22510b050cce174b2d39c364
Brian Campbell <bry123ftw@gmail.com>
2022-06-18 20:30:59 -0500

    installed MUI

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -26,21 +26,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
       <body>
-
+        <table>
+          <thead>
+            <tr>
+              <th>Distance</th>
+              <th>Time</th>
+              <th>Pace</th>
+              <th>Date</th>
+              <th>Category</th>
+              
+            </tr>
+          </thead>
+        </table>
       </body>
 
     </div>
